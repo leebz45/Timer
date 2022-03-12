e6475c18fd6e5093648c809d5fe9bf9d2716c7e1
leebz45 <43051856+leebz45@users.noreply.github.com>
2022-03-12 11:35:58 -0500

    Add to Git

---
+++ index.js
@@ -0,0 +1,23 @@
+const durationInput = document.getElementById("duration");
+const startButton = document.getElementById("startButton");
+const pauseButton = document.getElementById("pauseButton");
+const circle = document.querySelector('circle');
+
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray',perimeter);
+
+let duration;
+
+const timer = new Timer(durationInput,startButton,pauseButton,{
+    onStart(totalDuration) {
+        duration = totalDuration;
+    },
+    onTick(timeRemaining) {
+        circle.setAttribute('stroke-dashoffset',
+        perimeter * timeRemaining / duration - perimeter
+        );
+        
+    },
+    onComplete() {}
+});
+
